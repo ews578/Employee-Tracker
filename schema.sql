6a4e9401e0e5b06a1635b4bb27cc2021c66e1935
ews578 <ewsnider28@yahoo.com>
2024-03-11 11:28:05 -0500

    created schema and seeds sql

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE EMPLOEE_DB;
+CREATE DATABASE EMPLOEE_DB;
+USE EMPLOEE_DB;
+
+CREATE TABLE department (
+    id  INT     PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR (30)
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) references department(id)
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id) references role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) references employee(id) 
+);
---
+++ seeds.sql
@@ -0,0 +1,7 @@
+USE EMPLOEE_DB;
+
+INSERT INTO department (name) VALUES ("sales");
+
+INSERT INTO role (title,salary,department_id) VALUES ("manager",60000,1);
+
+INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES ("John","Sanders",1,NULL);
