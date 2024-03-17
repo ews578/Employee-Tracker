c109c5f06d2e7dde05ca492cebba10c228a941df
ews578 <ewsnider28@yahoo.com>
2024-03-17 14:26:37 -0500

    added info to seeds

--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,19 @@
 USE EMPLOYEE_DB;
 
-INSERT INTO department (name) VALUES ("sales");
+INSERT INTO department (name) 
+VALUES ("sales"),
+("install"),
+("service");
 
-INSERT INTO role (title,salary,department_id) VALUES ("manager",60000,1);
+INSERT INTO role (title,salary,department_id) 
+VALUES ("manager",75000,1),
+("Lead Install",60000,2),
+("Lead Service",60000,3);
 
-INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES ("John","Sanders",1,NULL);
+INSERT INTO employee (first_name,last_name,role_id)
+VALUES ("Tommy","Chong",1),
+("Cheech","Marion",1);
+INSERT INTO employee (first_name,last_name,role_id,manager_id) 
+VALUES ("John","Sanders",1,1),
+("Leroy","Jenkins",2, 2),
+("John B", "Goode",3,2);
